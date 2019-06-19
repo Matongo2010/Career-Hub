37d6d18bffce95d805e88307daa00ef0edd2c4fe
Matongo2010 <justineadriano638@gmail.com>
2019-06-19 16:36:57 +0300

    fetched upstream

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function details(name, email, ID, specialisation, certNumber, institution) {
+this.name = name;
+this.email = email;
+this.ID = ID;
+this.specialisation = specialisation;
+this.certNumber = certNumber;
+this.institution = institution;
+
+}
